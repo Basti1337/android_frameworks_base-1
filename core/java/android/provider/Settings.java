a90784968eb1a41b1d64f2ccff9c921f546ef02f
Dragon7780 <Dragon7780@hotmail.nl>
2014-02-08 05:52:30 +0100

    Settings: LS Notifications: Custom Background color [1/2]
    PS2: Update Author
         David96
    
    Change-Id: I0ed68479a32ddacbc10fc9eb766e07ee663e104c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4572,6 +4572,12 @@ public final class Settings {
         public static final String LOCKSCREEN_NOTIFICATIONS_EXCLUDED_APPS = "lockscreen_notifications_excluded_apps";
 
         /**
+         * Set a custom notification background color
+         * @hide
+         */
+        public static final String LOCKSCREEN_NOTIFICATIONS_COLOR = "lockscreen_notifications_color";
+
+        /**
          * Holds the text for the Carrier label. An empty string
          * will bring the default text back.
          * @hide
--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -80,6 +80,7 @@ public class NotificationViewManager {
         public int notificationsHeight = 4;
         public float offsetTop = 0.3f;
         public boolean privacyMode = false;
+        public int notificationColor = 0x55555555;
 
         public Configuration(Handler handler) {
             super(handler);
@@ -112,6 +113,8 @@ public class NotificationViewManager {
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_EXCLUDED_APPS), false, this);
+            notificationColor = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_COLOR, notificationColor);
         }
 
         @Override
