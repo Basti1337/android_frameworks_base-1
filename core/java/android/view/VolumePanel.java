3dd5977c9c29541979998c9c5f6480f5aac99046
Dragon7780 <Dragon7780@hotmail.nl>
2014-03-10 17:03:09 +0100

    [1/2] Base: Volume panel timeout
    Settings: https://gerrit.omnirom.org/#/c/5517/
    
    Change-Id: I15c4c589bde5794d7c961a02022a8e25a4e59500
    Signed-off-by: Dragon7780 <Dragon7780@hotmail.nl>

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -127,6 +127,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     private boolean mVoiceCapable;
     private boolean mVolumeLinkNotification;
     private int mCurrentOverlayStyle = -1;
+    private int mCustomTimeoutDelay = TIMEOUT_DELAY;
 
     private final boolean mTranslucentDialog;
     private boolean mShouldRunDropTranslucentAnimation = false;
@@ -249,6 +250,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
                     Settings.System.MODE_VOLUME_OVERLAY, VOLUME_OVERLAY_EXPANDABLE,
                     UserHandle.USER_CURRENT);
             changeOverlayStyle(overlayStyle);
+            mCustomTimeoutDelay = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.VOLUME_PANEL_TIMEOUT, TIMEOUT_DELAY);
         }
     };
 
@@ -374,6 +377,9 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         context.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.MODE_VOLUME_OVERLAY), false,
                 mSettingsObserver, UserHandle.USER_ALL);
+        context.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.VOLUME_PANEL_TIMEOUT), false,
+                mSettingsObserver);
 
         boolean masterVolumeKeySounds = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useVolumeKeySounds);
@@ -1129,7 +1135,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
 
     private void resetTimeout() {
         removeMessages(MSG_TIMEOUT);
-        sendMessageDelayed(obtainMessage(MSG_TIMEOUT), TIMEOUT_DELAY);
+        sendMessageDelayed(obtainMessage(MSG_TIMEOUT), mCustomTimeoutDelay);
     }
 
     private void forceTimeout() {
