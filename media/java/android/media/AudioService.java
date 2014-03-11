3dcf18e45886a55178666d5063a69192e6c2c29f
Andrzej Kaczmarek <andrzej.kaczmarek@tieto.com>
2014-03-11 08:40:44 +0100

    AudioService: Fix absolute volume for AVRCP
    Avrcp.java uses max volume obtained by getStreamMaxVolume() when
    converting to AVRCP levels and it returns non-scaled volume index.
    For this reason volume index needs to be dispatched to Avrcp before
    scaling as otherwise Avrcp will convert volume level incorrectly,
    i.e. max volume index for music stream is 15 and any volume index
    >1 (>10 after scaling) will result in setting *MAXIMUM* volume level
    on headset which is not safe for listener.
    
    Change-Id: Ia799eec578ed2326ec18b69f2ee4f2a60c664514

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -1124,8 +1124,6 @@ public class AudioService extends IAudioService.Stub {
 
             oldIndex = streamState.getIndex(device);
 
-            index = rescaleIndex(index * 10, streamType, streamTypeAlias);
-
             if (streamTypeAlias == AudioSystem.STREAM_MUSIC &&
                 (device & AudioSystem.DEVICE_OUT_ALL_A2DP) != 0 &&
                 (flags & AudioManager.FLAG_BLUETOOTH_ABS_VOLUME) == 0) {
@@ -1135,6 +1133,8 @@ public class AudioService extends IAudioService.Stub {
                     }
                 }
             }
+
+            index = rescaleIndex(index * 10, streamType, streamTypeAlias);
 
             flags &= ~AudioManager.FLAG_FIXED_VOLUME;
             if ((streamTypeAlias == AudioSystem.STREAM_MUSIC) &&
