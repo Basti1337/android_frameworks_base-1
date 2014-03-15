omnirom/android_frameworks_base_old

[1/2] Base: fix ActiveDisplay for WifiOnly Devices
fix http://jira.omnirom.org/browse/OMNI-637
also fix for devices with unsupported Proximity sensor and Light sensor

Change-Id: I346d4ed0ca02c6f8a6da1f3bf1de59ac9179d7b7

packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -69,6 +69,7 @@ import android.widget.LinearLayout;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.android.internal.util.omni.DeviceUtils;
 import com.android.internal.util.slim.QuietHoursHelper;
 import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.internal.widget.multiwaveview.GlowPadView.OnTriggerListener;
@@ -181,7 +182,7 @@ public class ActiveDisplayView extends FrameLayout
     private ProximitySensorManager mProximitySensorManager;
     private LightSensorManager mLightSensorManager;
 
-    private boolean mProximityIsFar = false;
+    private boolean mProximityIsFar = true;
     private boolean mIsInBrightLight = false;
     private boolean mWakedByPocketMode = false;
     private long mPocketTime = 0;
@@ -820,8 +821,10 @@ public class ActiveDisplayView extends FrameLayout
     private void handleShowNotificationView() {
         mIsActive = true;
         setVisibility(View.VISIBLE);
-        Log.i(TAG, "ActiveDisplay: enable LightSensor");
-        mLightSensorManager.enable();
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: enable LightSensor");
+            mLightSensorManager.enable();
+        }
         adjustStatusBarLocked(true);
         // Warm up KeyguardTouchDelegate so it's ready by the time
         KeyguardTouchDelegate.getInstance(mContext);
@@ -832,8 +835,10 @@ public class ActiveDisplayView extends FrameLayout
         restoreBrightness();
         mWakedByPocketMode = false;
         cancelTimeoutTimer();
-        Log.i(TAG, "ActiveDisplay: disable LightSensor");
-        mLightSensorManager.disable(true);
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: disable LightSensor");
+            mLightSensorManager.disable(true);
+        }
         adjustStatusBarLocked(false);
         setVisibility(View.GONE);
     }
@@ -843,8 +848,10 @@ public class ActiveDisplayView extends FrameLayout
         restoreBrightness();
         mWakedByPocketMode = false;
         cancelTimeoutTimer();
-        Log.i(TAG, "ActiveDisplay: disable LightSensor");
-        mLightSensorManager.disable(true);
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: disable LightSensor");
+            mLightSensorManager.disable(true);
+        }
         setVisibility(View.GONE);
     }
 
@@ -1310,7 +1317,18 @@ public class ActiveDisplayView extends FrameLayout
      * @return True if we should show this view.
      */
     private boolean shouldShowNotification() {
+        if (!hasProximitySensor()) {
+            return true;
+        }
         return mProximityIsFar;
+    }
+
+    private boolean hasProximitySensor() {
+        return DeviceUtils.deviceSupportsProximitySensor(mContext);
+    }
+
+    private boolean hasLightSensor() {
+        return DeviceUtils.deviceSupportsLightSensor(mContext);
     }
 
     /**

Basti1337/android_frameworks_base-1

Signed-off-by: dragon <dragon7780@hotmail.nl>

packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -69,6 +69,7 @@ import android.widget.LinearLayout;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.android.internal.util.fusion.DeviceUtils;
 import com.android.internal.util.fusion.QuietHoursHelper;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.multiwaveview.GlowPadView;
@@ -183,7 +184,7 @@ public class ActiveDisplayView extends FrameLayout
     private ProximitySensorManager mProximitySensorManager;
     private LightSensorManager mLightSensorManager;
 
-    private boolean mProximityIsFar = false;
+    private boolean mProximityIsFar = true;
     private boolean mIsInBrightLight = false;
     private boolean mWakedByPocketMode = false;
     private long mPocketTime = 0;
@@ -825,8 +826,10 @@ public class ActiveDisplayView extends FrameLayout
     private void handleShowNotificationView() {
         mIsActive = true;
         setVisibility(View.VISIBLE);
-        Log.i(TAG, "ActiveDisplay: enable LightSensor");
-        mLightSensorManager.enable();
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: enable LightSensor");
+            mLightSensorManager.enable();
+        }
         adjustStatusBarLocked(true);
         // Warm up KeyguardTouchDelegate so it's ready by the time
         KeyguardTouchDelegate.getInstance(mContext);
@@ -837,8 +840,10 @@ public class ActiveDisplayView extends FrameLayout
         restoreBrightness();
         mWakedByPocketMode = false;
         cancelTimeoutTimer();
-        Log.i(TAG, "ActiveDisplay: disable LightSensor");
-        mLightSensorManager.disable(true);
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: disable LightSensor");
+            mLightSensorManager.disable(true);
+        }
         adjustStatusBarLocked(false);
         setVisibility(View.GONE);
     }
@@ -848,8 +853,10 @@ public class ActiveDisplayView extends FrameLayout
         restoreBrightness();
         mWakedByPocketMode = false;
         cancelTimeoutTimer();
-        Log.i(TAG, "ActiveDisplay: disable LightSensor");
-        mLightSensorManager.disable(true);
+        if (hasLightSensor()) {
+            Log.i(TAG, "ActiveDisplay: disable LightSensor");
+            mLightSensorManager.disable(true);
+        }
         setVisibility(View.GONE);
 
     }
@@ -1324,7 +1331,18 @@ public class ActiveDisplayView extends FrameLayout
      * @return True if we should show this view.
      */
     private boolean shouldShowNotification() {
+        if (!hasProximitySensor()) {
+            return true;
+        }
         return mProximityIsFar;
+    }
+
+    private boolean hasProximitySensor() {
+        return DeviceUtils.deviceSupportsProximitySensor(mContext);
+    }
+
+    private boolean hasLightSensor() {
+        return DeviceUtils.deviceSupportsLightSensor(mContext);
     }
 
     /**